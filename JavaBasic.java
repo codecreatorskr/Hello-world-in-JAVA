75d5db039d2da238a70bb0145f167c051a9753c0
Saurabh Kumar <118014326+codecreaterskr@users.noreply.github.com>
2023-03-16 02:06:32 +0530

    Add files via upload

---
+++ JavaBasic.java
@@ -0,0 +1,9 @@
+public class JavaBasic{
+    public static void main(String args[]){
+        System.out.print("hello world\n");       //"\n" used for new line
+        System.out.print("hello world\n");
+        System.out.println("hello world");      //println for new line
+        System.out.print("hello world");
+        
+    }
+}
